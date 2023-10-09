dc9ac6847202763161c5046396b09911f925859a
joaoguilherme <joaoguilhermehig@gmail.com>
2023-10-09 18:29:11 -0300

    aula 04

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import './style.css';
-import Navbar from './components/NavBar/Navbar';
-
+import logo from './components/img/java.png';
 import './Reset.css';
 
-import Tasklist from './components/Tasklist/Tasklist';
-
-function App() {
+export default function App() {
   return (
     <div className='App'>
-      <Navbar />
-      <div>
-        <Tasklist title="Pendente"/>
-        <Tasklist title="Fazendo"/>
-        <Tasklist title="Completa"/>
-      </div>
+      <img src={logo}/>
     </div>
   );
 }
-
-export default App;
