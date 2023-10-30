60de56dc0ef7f823e2c908856881ba6539782a2d
joaoguilherme <joaoguilhermehig@gmail.com>
2023-10-30 18:15:41 -0300

    intalado o sass e uma função para mudar de pagina

--- src/Components/Janela/index.js
+++ src/Components/Janela/index.js
@@ -7,15 +7,19 @@ export default function Janela () {
     const [nome, setNome] = useState("");
     const [senha, setSenha] = useState("");
 
-  const handleSubmit = (event) => {
+  const submit = (event) => {
     event.preventDefault();
     alert(`Seu nome foi: ${nome} e sua senha foi ${senha}`)
+  }
+
+  const mudaPagina = () => {
+
   }
 
     return (
         <div className='content'>
             <img src={userIcone} className="userIcone" alt="UserIcone" />
-            <form name="cadastro" className='janela' onSubmit={handleSubmit}>
+            <form name="cadastro" className='janela' onSubmit={mudaPagina}>
 
                 <label for="usuario" className='nome'>Nome: </label>
                 <input type="text" name="usuario" value={nome} onChange={(e) => setNome(e.target.value)} className='nome-input'/>
@@ -23,7 +27,7 @@ export default function Janela () {
                 <label for="senha" className='senha'>Senha: </label>
                 <input type="password" name="senha" value={senha} onChange={(e) => setSenha(e.target.value)} className='senha-input'/>
 
-                <input type="submit" value="Entrar" className='entrar'/>
+                <input type="submit" value="Entrar" className='entrar' onClick={submit}/>
             </form>
             <h5><a href='index.html'>Esqueci a senha?</a></h5>
         </div>
