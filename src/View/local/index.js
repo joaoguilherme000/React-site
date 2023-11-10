b15383dbba9d7121996a1f06f63ff5935c8ee2d5
joaoguilherme <joaoguilhermehig@gmail.com>
2023-11-09 21:44:32 -0300

    Pagina da localizacao html feito

--- src/View/local/index.js
+++ src/View/local/index.js
@@ -1,10 +1,46 @@
 import Header from '../../Components/Header';
+import './index.css'
 
 export default function Local () {
     return (
         <>
         <Header/>
-        <label>Local</label>
+        <section className='processando'>
+            <div className='esquerda'>
+                <h2>Enviar</h2>
+                <h3>Certeza?</h3>
+                <div>
+                    <button>Sim</button>
+                    <button>Não</button>
+                </div>
+            </div>
+            <div className='direita'>
+                <h2>Recebido</h2>
+                <button>Documento</button>
+                <div>
+                    <button>Autorizar</button>
+                    <button>Refatorar</button>
+                </div>
+            </div>
+        </section>
+        <section className='lista'>
+        <div className='esquerda'>
+                <h2>Enviar</h2>
+                <h3>Certeza?</h3>
+                <div>
+                    <button>Sim</button>
+                    <button>Não</button>
+                </div>
+            </div>
+            <div className='direita'>
+                <h2>Recebido</h2>
+                <button>Documento</button>
+                <div>
+                    <button>Autorizar</button>
+                    <button>Refatorar</button>
+                </div>
+            </div>
+        </section>
         </>
     );
 }
