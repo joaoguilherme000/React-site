637b323a0f35c78dc501c48f1621d2a2023ddcc3
joaoguilherme <joaoguilhermehig@gmail.com>
2024-01-08 20:39:29 -0300

    projeto padrao

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Login from './View/Login';
-import NoPage from './View/NoPage';
-import Dashboard from './View/Dashboard';
-
-import Imagens from './View/Imagens';
-import Local from './View/Local';
-import Banco from './View/Banco/';
+import Home from './View/Home';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/Dashboard" element={<Dashboard/>} />
-        <Route path="/Imagens" element={<Imagens/>} />
-        <Route path="/Local" element={<Local/>} />
-        <Route path="/Banco" element={<Banco/>} />
-        <Route path="*" element={<NoPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Home/>
   );
 }
 
---
+++ src/View/Home/index.jsx
@@ -0,0 +1,10 @@
+import './index.css'
+
+export default function Home () {
+
+  return (
+    <main>
+      <h3>Ola</h3>
+    </main>
+    )
+};
